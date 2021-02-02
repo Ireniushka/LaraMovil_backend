da50a491cc4b979609d78ebe27542849fef07d89
Ireniushka <irenerengelruiz@gmail.com>
2021-02-02 11:57:12 +0100

    comentado gitignore

--- database/factories/AppliedFactory.php
+++ database/factories/AppliedFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Applied::class, function (Faker $faker) {
     return [
-        //
+        'offer_id' => \App\Offer::all()->random()->id,
+        'user_id' => \App\User::all()->random()->id,
+
     ];
 });
