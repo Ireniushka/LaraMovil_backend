eb52c14f1e740586e5ce129a6a5e94235a65c3a2
Ireniushka <irenerengelruiz@gmail.com>
2021-02-02 15:20:50 +0100

    seed v2
    arreglado seed para hacer el fresh bien

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Applied::class, 10)->create();
-        factory(\App\Article::class, 10)->create();
         factory(\App\Cicle::class, 10)->create();
+        factory(\App\Article::class, 10)->create();
         factory(\App\Offer::class, 10)->create();
         factory(\App\Requirement::class, 10)->create();
         factory(\App\User::class)->create(['email' => 'admin@damin.com', 'password'=>bcrypt('123456'), 'type'=>'ad']);
-
+        factory(\App\Applied::class, 10)->create();
+        
     }
 }
