a78fe904dab9a0a396c8a0484aca9d7f534cf70f
Ireniushka <irenerengelruiz@gmail.com>
2021-02-20 16:31:17 +0100

    informe oferta ordenado por ciclos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
     {
         factory(\App\Cicle::class, 10)->create();
         factory(\App\Article::class, 10)->create();
-        factory(\App\Offer::class, 10)->create();
+        factory(\App\Offer::class, 30)->create();
         factory(\App\Requirement::class, 10)->create();
         factory(\App\User::class)->create(['name' => 'Administrador', 'email' => 'admin@damin.com', 'password'=>bcrypt('123456'), 'type'=>'admin user']);
         factory(\App\User::class, 19)->create();
-        factory(\App\Applied::class, 10)->create();
+        factory(\App\Applied::class, 30)->create();
         
     }
 }
--- resources/views/generadorPdf/informesOfertas.blade.php
+++ resources/views/generadorPdf/informesOfertas.blade.php
@@ -25,31 +25,40 @@
 
         <p>Curso escolar: {{$curso}}</p>
         <br>
+        
+        @foreach($cicles as $cicle)
+            @foreach($offers as $offer)
+            @if($offer->cicle == $cicle)
+                <br>
+                <p>Ciclo de: {{$cicle->name}}</p>
 
-        <table class ="table table-light">
-            <thead class="thead-ligth">
-                <tr>
-                    <th>Oferta</th>
-                    <th>Descripción</th>
-                    <th>Ciclo</th>
-                    <th>Fecha</th>
-                    <th>Inscritos</th>
-                </tr>
-            </thead>
+                <table class ="table table-light">
+                    <thead class="thead-ligth">
+                        <tr>
+                            <th>Oferta</th>
+                            <th>Descripción</th>
+                            <th>Fecha</th>
+                            <th>Inscritos</th>
+                        </tr>
+                    </thead>
 
-            <tbody>
-                @foreach($offers as $offer)
-                    <tr>
-                        <td>{{$offer->headline}}</td>
-                        <td>{{$offer->description}}</td>
-                        <td>{{$offer->cicle->name}}</td>
-                        <td>{{$offer->date_max}}</td>
-                        <td>{{$offer->applieds()->count()}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>   
-    
+                    <tbody>
+                        @foreach($offers as $offer)
+                            @if($offer->cicle == $cicle)
+                                <tr>
+                                    <td>{{$offer->headline}}</td>
+                                    <td>{{$offer->description}}</td>
+                                    <td>{{$offer->date_max}}</td>
+                                    <td>{{$offer->applieds()->count()}}</td>
+                                </tr>
+                            @endif
+                        @endforeach
+                    </tbody>
+                </table>
+                @break
+            @endif
+            @endforeach   
+        @endforeach
 
     </body>
 </html>
